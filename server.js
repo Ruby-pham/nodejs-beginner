947c33a869aaf149d320529a1ba92ed0c0de27df
ruby pham <rubypham955@gmail.com>
2024-03-09 21:13:19 -0500

    add file

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const http = require("node:http");
-const hostname = "127.0.0.1";
-const port = 3000;
+const hostname = "localhost";
+const port = 6969;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World\n");
+  res.end("Hello World\n hoi dan IT with Eric");
 });
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
